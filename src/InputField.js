e8255fa50c3c715c3bb51db3442564ef82638588
Marlys Alvarado <sylram@icloud.com>
2023-10-17 13:13:10 -0400

    read, update, delete functionalities

--- src/InputField.js
+++ src/InputField.js
@@ -1,9 +1,6 @@
 
 import { Button } from 'react-bootstrap';
 import React from "react";
-// import Grid from "./Grid";
-
-
 
 class Inputfield extends React.Component {
   
@@ -17,30 +14,35 @@ class Inputfield extends React.Component {
       flag: false,
       personObj: {},
     };
+
     
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getPerson = this.fetchPerson.bind(this);
     this.getAll = this.fetchAll.bind(this);
+    this.updatePerson = this.handleUpdate.bind(this);
+    this.deletePerson = this.deletePerson.bind(this);
     
   }
 
 
   handleInputChange(event) {
-    const target = event.target;
+      const target = event.target;
       const value = target.value;
       const name = target.id;
       this.setState.personObj = ({...this.setState.personObj, [name]: value });
+      this.setState.idValue= value;
       console.log(this.setState.personObj);
      
   }
 
-  handleSubmit(event) {
+  handleUpdate(event) {
     this.setState.data = '';
     const fetchData = async() => {
       try {
-        const response = await fetch('http://localhost:8083//person-controller/create', {
-          method: 'POST',
+        const response = await fetch('http://localhost:8083/person-controller/update/' + this.setState.personObj.id, {
+          method: 'PUT',
           headers: {
             'Accept':'application/json',
             'Content-Type':'application/json',
@@ -68,6 +70,113 @@ class Inputfield extends React.Component {
     }
     fetchData();
     event.preventDefault();
+    this.setState.personObj={};
+    
+  }
+
+  handleSubmit(event) {
+    this.setState.data = '';
+    const fetchData = async() => {
+      try {
+        const response = await fetch('http://localhost:8083//person-controller/create', {
+          method: 'POST',
+          headers: {
+            'Accept':'application/json',
+            'Content-Type':'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+          body: JSON.stringify(this.setState.personObj),
+        });
+        if (!response.ok) {
+          this.setState.flag = false;
+          this.setState.data = 'Error creating Person';
+          this.forceUpdate();
+          this.setState.personObj = {};
+          return (<div>'Network response was not ok'</div>)
+          
+        }
+        const data = await response.json();
+        if (data) {
+          this.setState.data = [data];
+          this.setState.flag = true;
+          this.setState.idValue ='';
+          this.setState.nameValue = '';
+          this.setState.lastNameValue = '';
+          this.forceUpdate() // force re-render component
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        this.setState.flag = false;
+        this.setState.data = 'Error creating Person';
+        this.forceUpdate();
+      }
+    }
+    fetchData();
+    event.preventDefault();
+  
+    
+  }
+
+  fetchPerson(event) {
+    this.setState.data = '';
+    const fetchData = async() => {
+      try {
+        const response = await fetch('http://localhost:8083/person-controller/read/' + this.setState.personObj.id, {
+          method: 'GET',
+        });
+        if (!response.ok) {
+          this.setState.flag = false;
+          this.setState.data = 'Network response was not ok';
+          this.forceUpdate();
+          throw new Error('Network response was not ok');
+          
+        }
+        const data = await response.json();
+        if (data) {
+          this.setState.data = [data];
+          this.setState.flag = true;
+          this.forceUpdate() // force re-render component
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        this.setState.flag = false;
+        this.setState.data = error.message;
+        this.forceUpdate();
+      }
+    }
+    fetchData();
+    event.preventDefault();
+  }
+
+  deletePerson(event) {
+    this.setState.data = '';
+    const deleteData = async() => {
+      try {
+        const response = await fetch('http://localhost:8083/person-controller/delete/' + this.setState.personObj.id, {
+          method: 'DELETE',
+        });
+        if (!response.ok) {
+          this.setState.flag = false;
+          this.setState.data = 'Network response was not ok';
+          this.forceUpdate();
+          throw new Error('Network response was not ok');
+          
+        }
+        const data = await response.json();
+        if (data) {
+          this.setState.flag = false;
+          this.setState.data = 'Person has been deleted -> ' + data.firstName + " " + data.lastName;
+          this.forceUpdate();
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        this.setState.flag = false;
+        this.setState.data = error.message;
+        this.forceUpdate();
+      }
+    }
+   deleteData();
+    event.preventDefault();
   }
 
   fetchAll(event) {
@@ -81,8 +190,8 @@ class Inputfield extends React.Component {
           this.setState.flag = false;
           this.setState.data = 'Network response was not ok';
           this.forceUpdate();
-          return (<div>'Network response was not ok'</div>)
-          // throw new Error('Network response was not ok');
+          // return (<div>'Network response was not ok'</div>)
+          throw new Error('Network response was not ok');
           
         }
         const data = await response.json();
@@ -93,6 +202,9 @@ class Inputfield extends React.Component {
         }
       } catch (error) {
         console.error('Error:', error);
+        this.setState.flag = false;
+        this.setState.data = error.message;
+        this.forceUpdate();
       }
     }
     fetchData();
@@ -102,7 +214,7 @@ class Inputfield extends React.Component {
   render() {
     return (
       <><div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={(ev) => ev.target.reset()} >
           <label>
             Id:
             <br></br>
@@ -118,9 +230,11 @@ class Inputfield extends React.Component {
             <br></br>
             <input type="text" id="lastName" onChange={this.handleInputChange} />
           </label><br></br>
-          {/* <button className="button" type="submit" value="Submit" /> */}
           <Button className="button" onClick={this.handleSubmit}>Create</Button>
+          <Button className="button" onClick={this.getPerson}>Read</Button>
+          <Button className="button" onClick={this.updatePerson}>Update</Button>
           <Button className="button" onClick={this.getAll}>Read All</Button>
+          <Button className="button" onClick={this.deletePerson}>Delete</Button>
 
         </form>
       </div>
@@ -134,18 +248,15 @@ class Inputfield extends React.Component {
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
             </tr> 
-            {this.setState.data.map((item, index) => (
+            {this.setState.data.length ? this.setState.data.map((item, index) => (
               <tr key={index}>
               <td>{item.id}</td>
               <td>{item.firstName}</td>
               <td>{item.lastName}</td>
               </tr>
-            ))}
+            )) : <span></span>}  
            </tbody>}
         </table>
-        
-          {/* <div className='error'>{this.setState.data}</div> */}
-         
         </div>
         {this.setState.flag === false && <div  className='error'>{this.setState.data}</div> }
         </>
